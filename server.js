ef59bce766b8558218934a249e22c81fd8914c4e
Alex <Alexander.Ivanov.GDC@gmail.com>
2017-06-19 10:06:14 +0300

    Working 1

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000');
 });
